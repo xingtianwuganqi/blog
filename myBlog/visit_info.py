c1c4a3a2b0d23c5dd960c79dce05133b56be888c
jingjun <rxswift@126.com>
2020-11-26 13:58:41 +0800

    fix: 修复ip统计

--- myBlog/visit_info.py
+++ myBlog/visit_info.py
@@ -19,6 +19,7 @@ def change_info(request, end_point):
     count_nums.save()
 
     # 记录访问 ip 和每个 ip 的次数
+    # client_ip = ""
     if 'HTTP_X_FORWARDED_FOR' in request.META:  # 获取 ip
         client_ip = request.META['HTTP_X_FORWARDED_FOR']
         client_ip = client_ip.split(",")[0]  # 所以这里是真实的 ip
@@ -31,10 +32,7 @@ def change_info(request, end_point):
         uobj = ip_exist[0]
         uobj.count += 1
     else:
-        uobj = UserIP()
-        uobj.ip = client_ip
-        uobj.end_point = end_point
-        uobj.count = 1
+        uobj = UserIP.objects.create(ip=client_ip,end_point=end_point,count=1)
     uobj.save()
 
     # 增加今日访问次数
